c48485a292ed8d8a570aabcebc32dd5fca7e28c2
iromanro <iroirogames@gmail.com>
2020-04-19 13:49:21 -0600

    fix logout

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -8,6 +8,10 @@ export const MainNav = (props) => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.global.user);
 
+  useEffect(() => {
+
+  }, [user])
+
   const discordLogin = () => {
     window.location.href = `https://discordapp.com/api/oauth2/authorize?client_id=${process.env.REACT_APP_DISCORD_CLIENT_ID}&redirect_uri=https%3A%2F%2Fnookling-showcase-fe.herokuapp.com%2Fauth&response_type=code&scope=identify%20email`;
   }
@@ -16,18 +20,24 @@ export const MainNav = (props) => {
     dispatch(userLogout())
   }
 
+  const goToPage = (page) => {
+    history.push(page);
+  }
+
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Nookling Showcase</Navbar.Brand>
+        <Navbar.Brand onClick={() => goToPage('/')}>Nookling Showcase</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Homes</Nav.Link>
-            <Nav.Link href="#link">Custom Patterns</Nav.Link>
+            <Nav.Link onClick={() => goToPage('/homes')}>Homes</Nav.Link>
+            <Nav.Link onClick={() => goToPage('/patterns')}>Custom Patterns</Nav.Link>
             {!user.isAuthenticated ?
               <Nav.Link onClick={() => discordLogin()}>Log in</Nav.Link>
               :
+              <Nav.Link onClick={() => goToPage('/profile')}>Profile</Nav.Link>
+              <Nav.Link onClick={() => goToPage('/settings')}>Settings</Nav.Link>
               <Nav.Link onClick={() => logout()}>Log out</Nav.Link>
             }
           </Nav>
