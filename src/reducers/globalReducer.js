ac479d2f4a1120dc877bb45fc043b4023ce9709d
iromanro <iroirogames@gmail.com>
2020-04-18 22:42:19 -0600

    DIx crash

--- src/reducers/globalReducer.js
+++ src/reducers/globalReducer.js
@@ -22,7 +22,7 @@ const globalReducer = (
       };
     }
     case 'USER_LOGIN_FULFILLED': {
-      console.log("login: ", action.payload);
+      console.log("login: ", action);
       setAuthorizationToken(action.payload.data.jwt)
       var userToken = jwt.decode(action.payload.data.jwt)
       localStorage.set('jwt', action.payload.data.jwt)
