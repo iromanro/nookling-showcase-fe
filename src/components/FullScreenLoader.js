efaac2c56b6884060443fec01c75be65ab72ce60
iromanro <iroirogames@gmail.com>
2020-04-18 22:06:38 -0600

    auth fixes

--- src/components/FullScreenLoader.js
+++ src/components/FullScreenLoader.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Spinner from 'react-bootstrap/Spinner'
 
 export const FullScreenLoader = (props) => {
 
   return (
     <div className="full-screen-loader">
       <div className="d-flex justify-content-center">
-        <div className="spinner-grow loader" role="status">
-          <span className="sr-only">Loading</span>
-        </div>
+        <Modal show={true}>
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </Modal>
       </div>
     </div>
   ); 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import '../../styles/main.scss';
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import MainNav from '../MainNav'
-import FilterBar from '../FilterBar';
-import ResultsList from '../ResultsList';
+import FilterBar from '../FilterBar'
+import ResultsList from '../ResultsList'
 
 export const Home = (props) => {
   const dispatch = useDispatch();
